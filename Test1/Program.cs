e70367c0aadf7126197c38d1e47a6cc01c1543f5
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-01-03 15:01:35 +0300

    Задание 1

---
+++ Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+ Console.WriteLine("Большее число = " + num1);
+}
+else if (num1 < num2)
+{
+ Console.WriteLine("Большее число = " + num2);
+}
+else if (num1 == num2)
+{
+ Console.WriteLine("Числа равны");
+}
+else
+{
+ Console.WriteLine("Неверный ввод");
+}
